b73aba7c5ccb4662387a2cd699708128c3c7e6f1
lapointek <156107790+lapointek@users.noreply.github.com>
2025-03-08 18:08:46 -0500

    added exercise-4

---
+++ exercise-4/customer.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Customer = void 0;
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    Customer.prototype.greeter = function () {
+        console.log("Hello ".concat(this.firstName, " ").concat(this.lastName));
+    };
+    return Customer;
+}());
+exports.Customer = Customer;
+var customer = new Customer("John", "Smith");
+customer.greeter();
